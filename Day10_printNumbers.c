7b3c4870e26eda1051817564075e494fde42a7cf
codewithharshil05 <harshilbohra7@gmail.com>
2026-07-03 13:33:19 +0530

    Create Day 10: print numbers from 1 to N using for loop

---
+++ Day10_printNumbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    
+    printf("Numbers from 1 to %d:\n", n);
+    
+    for(i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+    
+    return 0;
+}
